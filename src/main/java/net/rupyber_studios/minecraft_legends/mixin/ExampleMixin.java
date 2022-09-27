29b260dd6a1cb970f334b6efd8d5d9a3f1123e8f
Pyrix25633 <biral.mattia@gmail.com>
2022-09-27 15:32:26 +0200

    Initial commit

---
+++ src/main/java/net/rupyber_studios/minecraft_legends/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.rupyber_studios.minecraft_legends.mixin;
+
+import net.rupyber_studios.minecraft_legends.MinecraftLegends;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		MinecraftLegends.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
